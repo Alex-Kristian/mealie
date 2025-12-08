73ba8bbc3f49f23c5ac3517b757ae643415badbd
Alex-Kristian <2020alexk@gmail.com>
2025-12-08 03:22:03 +0000

    Add generate recipe function and route to AIApi

--- frontend/lib/api/user/ai.ts
+++ frontend/lib/api/user/ai.ts
@@ -1,14 +1,21 @@
 import { BaseAPI } from "../base/base-clients";
+import type { Recipe } from "~/lib/api/types/recipe";
 
 const prefix = "/api/recipes/ai";
 
 const routes = {
     generateNutrition: (recipeId: string) =>
         `${prefix}/generate-nutrition/${recipeId}`,
+    generateRecipe: () =>
+        `${prefix}/generate-recipe`,
 };
 
 export class AiApi extends BaseAPI {
     async generateNutrition(recipeId: string) {
         return await this.requests.post(routes.generateNutrition(recipeId), {});
+    }
+
+    async generateRecipe(userPrompt: string) {
+        return await this.requests.post<Recipe>(routes.generateRecipe(), { userPrompt: userPrompt });
     }
 }
