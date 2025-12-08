33e61392343dde724ba6753cf9cf4eab5a50e7b4
Alex-Kristian <2020alexk@gmail.com>
2025-12-08 03:24:11 +0000

    Add generate recipe to RecipeAiController

--- mealie/routes/recipe/ai.py
+++ mealie/routes/recipe/ai.py
@@ -1,10 +1,17 @@
+from pydantic import BaseModel
+
 from mealie.routes._base import BaseUserController, controller
 from mealie.routes._base.routers import UserAPIRouter
+from mealie.schema.recipe.recipe import Recipe
 from mealie.schema.recipe.recipe_nutrition import Nutrition
 from mealie.services.openai.nutrition import NutritionAIService
 from mealie.services.recipe.recipe_service import RecipeService
 
 router = UserAPIRouter(prefix="/ai")
+
+
+class GenerateRecipeRequest(BaseModel):
+    userPrompt: str
 
 
 @controller(router)
@@ -22,3 +29,10 @@ class RecipeAiController(BaseUserController):
         )
 
         return updated_recipe.nutrition
+
+    @router.post("/generate-recipe", response_model=Recipe)
+    async def generate_recipe_from_text(self, request: GenerateRecipeRequest) -> Recipe:
+        recipe_service = RecipeService(
+            repos=self.repos, user=self.user, household=self.household, translator=self.translator
+        )
+        return await recipe_service.create_from_text(user_prompt=request.userPrompt)
